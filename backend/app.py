23fe2ab7d83b094030410c5d6dc05551d64b6c1a
spurthi <mailaramspurthi10@gmail.com>
2026-06-17 23:25:52 +0530

    Add more songs for mood recommendations

--- backend/app.py
+++ backend/app.py
@@ -26,30 +26,72 @@ SONGS: Dict[str, List[Song]] = {
             "artist": "Armaan Malik",
             "link": "https://www.youtube.com/results?search_query=Butta+Bomma",
         },
+        {
+            "title": "Samajavaragamana",
+            "artist": "Sid Sriram",
+            "link": "https://www.youtube.com/results?search_query=Samajavaragamana",
+        },
+        {
+            "title": "Kesariya",
+            "artist": "Arijit Singh",
+            "link": "https://www.youtube.com/results?search_query=Kesariya",
+        },
     ],
+
     "sad": [
         {
             "title": "Someone Like You",
             "artist": "Adele",
             "link": "https://www.youtube.com/results?search_query=Someone+Like+You+Adele",
         },
+        {
+            "title": "Agar Tum Saath Ho",
+            "artist": "Alka Yagnik",
+            "link": "https://www.youtube.com/results?search_query=Agar+Tum+Saath+Ho",
+        },
+        {
+            "title": "Adiga Adiga",
+            "artist": "Sid Sriram",
+            "link": "https://www.youtube.com/results?search_query=Adiga+Adiga",
+        },
     ],
+
     "relaxed": [
         {
             "title": "Weightless",
             "artist": "Marconi Union",
             "link": "https://www.youtube.com/results?search_query=Weightless+Marconi+Union",
         },
+        {
+            "title": "Inkem Inkem Inkem Kaavaale",
+            "artist": "Sid Sriram",
+            "link": "https://www.youtube.com/results?search_query=Inkem+Inkem+Inkem+Kaavaale",
+        },
+        {
+            "title": "Raataan Lambiyan",
+            "artist": "Jubin Nautiyal",
+            "link": "https://www.youtube.com/results?search_query=Raataan+Lambiyan",
+        },
     ],
+
     "energetic": [
         {
             "title": "Believer",
             "artist": "Imagine Dragons",
             "link": "https://www.youtube.com/results?search_query=Believer+Imagine+Dragons",
         },
+        {
+            "title": "Naatu Naatu",
+            "artist": "Rahul Sipligunj",
+            "link": "https://www.youtube.com/results?search_query=Naatu+Naatu",
+        },
+        {
+            "title": "Malhari",
+            "artist": "Vishal Dadlani",
+            "link": "https://www.youtube.com/results?search_query=Malhari",
+        },
     ],
 }
-
 
 @app.route("/")
 def home():
