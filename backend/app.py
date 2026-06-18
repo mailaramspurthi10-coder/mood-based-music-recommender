78aff80971aec77de4311928086086802612c08c
spurthi <mailaramspurthi10@gmail.com>
2026-06-18 10:44:23 +0530

    Fix static JS linking for Render

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from typing import TypedDict, List, Dict
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 CORS(app)
 
 
